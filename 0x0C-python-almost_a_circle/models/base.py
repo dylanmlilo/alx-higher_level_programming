d9081cd2af8bf2a12ad82dd82c0b286becd98717
dylanmlilo <dylanmlilo12@gmail.com>
2023-06-23 03:19:16 -0700

    python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 
 import json
 import csv
+from os import path
 
 
 class Base:
@@ -20,3 +21,108 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of list_dictionaries
+
+        Args:
+            list_dictionaries (list): a list of dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes the JSON string representation of list_objs to a file
+
+        Args:
+            list_objs (list): a list of objects
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns the list of the JSON string representation json_string
+
+        Args:
+            json_string (str): A JSON str representation of a list of dicts
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes already set
+
+        Args:
+            **dictionary (dict): Key/value pairs of attributes to initialise
+        """
+        if cls.__name__ == "Square":
+            dummy = cls(3)
+
+        if cls.__name__ == "Rectangle":
+            dummy = cls(3, 3)
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """ returns a list of classes instantiated
+            from a file of JSON strings """
+
+        filename = cls.__name__ + '.json'
+
+        if path.exists(filename) is False:
+            return []
+
+        with open(filename, mode='r', encoding='utf-8') as jsonfile:
+            objs = cls.from_json_string(jsonfile.read())
+            instances = []
+            for elem in objs:
+                instances.append(cls.create(**elem))
+            return instances
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ writes the CSV serialization of a list of objects to a file """
+
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline="") as csvfile:
+            if list_objs is None or list_objs == []:
+                csvfile.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    keyNames = ["id", "width", "height", "x", "y"]
+                else:
+                    keyNames = ["id", "size", "x", "y"]
+                csvWriter = csv.DictWriter(csvfile, keyNames=keyNames)
+                for obj in list_objs:
+                    csvWritewriter.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ returns a list of classes instantiated from a CSV file """
+
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    keyNames = ["id", "width", "height", "x", "y"]
+                else:
+                    keyNames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, keyNames=keyNames)
+                list_dicts = [dict([i, int(j)] for i, j in d.items())
+                              for k in list_dicts]
+                return [cls.create(**k) for k in list_dicts]
+        except IOError:
+            return []
